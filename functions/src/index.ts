325b9906b2d8540755aea0c0bd5882cbbfcea376
Steven Schaner <schanerst@gmail.com>
2020-04-17 15:14:20 -0400

    update function that changes Steven to Memphis and create a function that tries to send an email from the contact form

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,17 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as sgMail from "@sendgrid/mail";
+
+admin.initializeApp();
+
+const API_KEY = functions.config().sendgrid.key;
+const TEMPLATE_ID = functions.config().sendgrid.template;
+sgMail.setApiKey(API_KEY);
 
 export const onMessageCreate = functions.database
   .ref("users/{arrayId}/userContactInformation")
-  .onCreate((snapshot, context) => {
-    const arrayId = context.params.arrayId;
-    console.log(`The array element of this user is ${arrayId}`);
-
+  .onCreate((snapshot) => {
     const messageData = snapshot.val();
-    console.log(messageData.firstName);
     const firstName = addPizzazz(messageData.firstName);
     return snapshot.ref.update({ firstName: firstName });
   });
@@ -15,3 +19,21 @@ export const onMessageCreate = functions.database
 function addPizzazz(firstName: string): string {
   return firstName.replace(/\bSteven\b/g, "Memphis");
 }
+
+export const welcomeEmail = functions.database
+  .ref("users/{arrayId}")
+  .onCreate((snapshot) => {
+    const messageData = snapshot.val();
+    const userEmail = messageData.userContactInformation.email;
+    const userFirstName = messageData.userContactInformation.firstName;
+    const msg = {
+      to: userEmail,
+      from: "info@waveimpactcapitalgroud.com",
+      templateId: TEMPLATE_ID,
+      dynamic_template_data: {
+        subject: "Thank You for Contacting Us for More Information.",
+        name: userFirstName,
+      },
+    };
+    return sgMail.send(msg);
+  });
